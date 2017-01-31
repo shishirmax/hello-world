be162748a55eb7c136c7dcb0b2e47d4ca6c6a8ca
shishirmax <shishirmax@gmail.com>
2017-01-31 11:31:38 +0530

    removed commented code

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-// var app = require('app');
-// var BrowserWindow = require('browser-window');
-
-// var mainWindow = null;
-
-// app.on('window-all-closed',function(){
-//     if(process.platform != 'darwin'){
-//         app.quit();
-//     }
-// });
-
-// app.on('ready',function(){
-//     mainWindow = new BrowserWindow({width:900,height:600});
-//     mainWindow.loadURL('file://'+__dirname+'/index.html');
-
-//     mainWindow.on('closed',function(){
-//         mainWindow = null;
-//     });
-// });
-
 const electron = require('electron');
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
